9295f43a55169bfa55b94395e21b53cd10bd093c
Anthony Graca <anthonygraca@gmail.com>
2021-06-25 15:38:16 -0700

    added first test for first question

---
+++ src/main/cpp/easy/1-two-sum.h
@@ -0,0 +1,16 @@
+#ifndef LEETCODE_EASY_1_TWO_SUM_H_
+#define LEETCODE_EASY_1_TWO_SUM_H_
+
+#include <vector>
+
+namespace leetcode {
+class TwoSum {
+  public:
+  std::vector<int> twoSum(std::vector<int> nums, int target) {
+    std::vector<int> answer{0,1};
+    return answer;
+  };
+};
+} // namespace leetcode
+
+#endif
--- src/test/cpp/easy/1-two-sum-test.cpp
+++ src/test/cpp/easy/1-two-sum-test.cpp
@@ -1,3 +1,11 @@
 #include "easy/1-two-sum.h"
 
 #include "gtest/gtest.h"
+
+TEST(TwoSum, InitializedStackIsEmpty) {
+  std::vector<int> nums{2, 7, 11, 15};
+  int target{9};
+  leetcode::TwoSum solution;
+  std::vector<int> expected{0,1};
+  ASSERT_EQ(expected, solution.twoSum(nums, target));
+}
