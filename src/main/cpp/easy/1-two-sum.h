785494b58e406ccc158cae0a7a765c186d1b6479
Anthony Graca <anthonygraca@gmail.com>
2021-06-25 19:54:45 -0700

    solved problem 1

--- src/main/cpp/easy/1-two-sum.h
+++ src/main/cpp/easy/1-two-sum.h
@@ -1,13 +1,44 @@
 #ifndef LEETCODE_EASY_1_TWO_SUM_H_
 #define LEETCODE_EASY_1_TWO_SUM_H_
 
+/*
+  Given an array of integers nums and an integer target, return indices of the 
+  two numbers such that they add up to target.
+
+  You may assume that each input would have exactly one solution, and you may 
+  not use the same element twice.
+
+  You can return the answer in any order.
+
+  Constraints:
+
+  2 <= nums.length <= 104
+  -109 <= nums[i] <= 109
+  -109 <= target <= 109
+  Only one valid answer exists.
+*/
+
 #include <vector>
+#include <unordered_map>
+#include <utility>
 
 namespace leetcode {
 class TwoSum {
   public:
   std::vector<int> twoSum(std::vector<int> nums, int target) {
-    std::vector<int> answer{0,1};
+    std::vector<int> answer;
+    std::unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++) {
+      int desired_num = target - nums[i];
+      if (map.find(desired_num) == std::end(map)) {
+        map.insert(std::make_pair(nums[i], i));
+      }
+      else {
+	answer.push_back(map.find(desired_num)->second);
+        answer.push_back(i);
+	return answer;
+      }
+    }
     return answer;
   };
 };
--- src/test/cpp/easy/1-two-sum-test.cpp
+++ src/test/cpp/easy/1-two-sum-test.cpp
@@ -2,9 +2,25 @@
 
 #include "gtest/gtest.h"
 
-TEST(TwoSum, InitializedStackIsEmpty) {
+TEST(TwoSum, FirstExample) {
   std::vector<int> nums{2, 7, 11, 15};
   int target{9};
+  leetcode::TwoSum solution;
+  std::vector<int> expected{0,1};
+  ASSERT_EQ(expected, solution.twoSum(nums, target));
+}
+
+TEST(TwoSum, SecondExample) {
+  std::vector<int> nums{3, 2, 4};
+  int target{6};
+  leetcode::TwoSum solution;
+  std::vector<int> expected{1,2};
+  ASSERT_EQ(expected, solution.twoSum(nums, target));
+}
+
+TEST(TwoSum, ThirdExample) {
+  std::vector<int> nums{3, 3};
+  int target{6};
   leetcode::TwoSum solution;
   std::vector<int> expected{0,1};
   ASSERT_EQ(expected, solution.twoSum(nums, target));
