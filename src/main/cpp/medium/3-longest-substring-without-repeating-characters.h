1062b1588234915060c05f2b96382dac42af03c7
Anthony Graca <anthonygraca@gmail.com>
2021-06-26 20:28:55 -0700

    added tests to q3

--- src/main/cpp/medium/3-longest-substring-without-repeating-characters.h
+++ src/main/cpp/medium/3-longest-substring-without-repeating-characters.h
@@ -1,4 +1,25 @@
 #ifndef LEETCODE_MEDIUM_3_LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS_H_
 #define LEETCODE_MEDIUM_3_LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS_H_
 
+/*
+  Given a string s, find the length of the longest substring without repeating 
+  characters.
+
+  Constraints:
+
+  * 0 <= s.length <= 5 * 104
+  * s consists of English letters, digits, symbols and spaces.
+*/
+
+#include <string>
+
+namespace leetcode {
+class LongestSubstring {
+  public:
+    int lengthOfLongestSubstring(std::string s) {
+      return 0;
+    }
+};
+} // namespace leetcode
+
 #endif
--- src/test/cpp/medium/3-longest-substring-without-repeating-characters-test.cpp
+++ src/test/cpp/medium/3-longest-substring-without-repeating-characters-test.cpp
@@ -2,6 +2,28 @@
 
 #include "gtest/gtest.h"
 
+/*
 TEST(LongestSubstringWithoutRepeatingCharacters, FirstExample) {
-  ASSERT_EQ(true, true);
+  std::string input{"abcabcbb"};
+  leetcode::LongestSubstring solution;
+  ASSERT_EQ(3, solution.lengthOfLongestSubstring(input));
+}
+
+TEST(LongestSubstringWithoutRepeatingCharacters, SecondExample) {
+  std::string input{"bbbbb"};
+  leetcode::LongestSubstring solution;
+  ASSERT_EQ(1, solution.lengthOfLongestSubstring(input));
+}
+
+TEST(LongestSubstringWithoutRepeatingCharacters, ThirdExample) {
+  std::string input{"pkwwkew"};
+  leetcode::LongestSubstring solution;
+  ASSERT_EQ(3, solution.lengthOfLongestSubstring(input));
+}
+*/
+
+TEST(LongestSubstringWithoutRepeatingCharacters, FourthExample) {
+  std::string input{""};
+  leetcode::LongestSubstring solution;
+  ASSERT_EQ(0, solution.lengthOfLongestSubstring(input));
 }
