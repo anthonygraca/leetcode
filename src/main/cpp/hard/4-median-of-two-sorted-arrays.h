c4517f44302da7822913deec08d40f308bc4c3c1
Anthony Graca <anthonygraca@gmail.com>
2021-07-01 22:10:22 -0700

    updated readme and added more tests

--- src/main/cpp/hard/4-median-of-two-sorted-arrays.h
+++ src/main/cpp/hard/4-median-of-two-sorted-arrays.h
@@ -1,6 +1,13 @@
 #ifndef LEETCODE_HARD_4_MEDIAN_OF_TWO_SORTED_ARRAYS_H_
 #define LEETCODE_HARD_4_MEDIAN_OF_TWO_SORTED_ARRAYS_H_
 
+/*
+  Given two sorted arrays nums1 and nums2 of size m and n respectively, return 
+  the median of the two sorted arrays.
+
+  The overall run time complexity should be O(log (m+n)).
+ */
+
 #include <vector>
 
 namespace leetcode {
--- src/test/cpp/hard/4-median-of-two-sorted-arrays-test.cpp
+++ src/test/cpp/hard/4-median-of-two-sorted-arrays-test.cpp
@@ -2,9 +2,43 @@
 
 #include "gtest/gtest.h"
 
+#include <vector>
+
+/*
+TEST(MedianOfTwoSortedArrays, FirstExample) {
+  std::vector<int> nums1{1, 3};
+  std::vector<int> nums2{2};
+  leetcode::MedianOfTwoSortedArrays solution;
+  ASSERT_EQ(2.0, solution.findMedianSortedArrays(nums1, nums2));
+}
+
+TEST(MedianOfTwoSortedArrays, SecondExample) {
+  std::vector<int> nums1{1, 2};
+  std::vector<int> nums2{3, 4};
+  leetcode::MedianOfTwoSortedArrays solution;
+  ASSERT_EQ(2.5, solution.findMedianSortedArrays(nums1, nums2));
+}
+
+TEST(MedianOfTwoSortedArrays, ThirdExample) {
+  std::vector<int> nums1{0, 0};
+  std::vector<int> nums2{0, 0};
+  leetcode::MedianOfTwoSortedArrays solution;
+  ASSERT_EQ(0, solution.findMedianSortedArrays(nums1, nums2));
+}
+*/
+
 TEST(MedianOfTwoSortedArrays, FourthExample) {
   std::vector<int> nums1{};
   std::vector<int> nums2{1};
   leetcode::MedianOfTwoSortedArrays solution;
   ASSERT_EQ(1.0, solution.findMedianSortedArrays(nums1, nums2));
 }
+
+/*
+TEST(MedianOfTwoSortedArrays, FifthExample) {
+  std::vector<int> nums1{2};
+  std::vector<int> nums2{};
+  leetcode::MedianOfTwoSortedArrays solution;
+  ASSERT_EQ(2.0, solution.findMedianSortedArrays(nums1, nums2));
+}
+*/
