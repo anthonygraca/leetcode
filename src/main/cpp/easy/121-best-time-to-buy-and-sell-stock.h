630f2979dc9bcb57f0eaede1b5353a8f47d03302
Anthony Graca <anthonygraca@gmail.com>
2021-07-08 22:51:01 -0700

    fixed compile issues

--- src/main/cpp/easy/121-best-time-to-buy-and-sell-stock.h
+++ src/main/cpp/easy/121-best-time-to-buy-and-sell-stock.h
@@ -1,4 +1,15 @@
 #ifndef LEETCODE_EASY_121_BEST_TIME_TO_BUY_AND_SELL_STOCK_H_
 #define LEETCODE_EASY_121_BEST_TIME_TO_BUY_AND_SELL_STOCK_H_
 
+#include <vector>
+
+namespace leetcode {
+class BestTimeToBuyAndSellStock {
+public:
+  int maxProfit(std::vector<int>& nums) {
+    return 5;
+  }
+};
+} // namespace leetcode
+
 #endif
