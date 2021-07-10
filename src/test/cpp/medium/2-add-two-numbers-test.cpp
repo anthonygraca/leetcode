56914aaab9f6ecfe766c1078cfaf9fd03586f03f
Anthony Graca <anthonygraca@gmail.com>
2021-07-10 11:31:22 -0700

    refractored test for problem #2

--- src/test/cpp/medium/2-add-two-numbers-test.cpp
+++ src/test/cpp/medium/2-add-two-numbers-test.cpp
@@ -4,6 +4,8 @@
 
 #include <vector>
 
+#include "util/linked-list.h"
+
 class AddTwoNumbers : public ::testing::Test {
   protected:
     void TearDown() override {
@@ -20,56 +22,35 @@ class AddTwoNumbers : public ::testing::Test {
     void releaseNodes(leetcode::ListNode* list) {
       while (list != nullptr) {
         leetcode::ListNode* currentNode = list;
-	list = list->next;
-	delete currentNode;
+        list = list->next;
+        delete currentNode;
       }
     }
 };
 
-leetcode::ListNode* getLinkedListForm(std::vector<int>);
-bool isEqual(leetcode::ListNode*, leetcode::ListNode*);
-
 TEST_F(AddTwoNumbers, FirstExample) {
-  l1 = getLinkedListForm({2, 4, 3});
-  l2 = getLinkedListForm({5, 6, 4});
-  expected_output = getLinkedListForm({7,0,8});
+  l1 = leetcode::getLinkedListForm({2, 4, 3});
+  l2 = leetcode::getLinkedListForm({5, 6, 4});
+  expected_output = leetcode::getLinkedListForm({7,0,8});
   leetcode::AddTwoNumbers solution;
   actual_output = solution.addTwoNumbers(l1, l2);
-  ASSERT_TRUE(isEqual(expected_output, actual_output));
+  ASSERT_TRUE(leetcode::isEqual(expected_output, actual_output));
 }
 
 TEST_F(AddTwoNumbers, SecondExample) {
-  l1 = getLinkedListForm({0});
-  l2 = getLinkedListForm({0});
-  expected_output = getLinkedListForm({0});
+  l1 = leetcode::getLinkedListForm({0});
+  l2 = leetcode::getLinkedListForm({0});
+  expected_output = leetcode::getLinkedListForm({0});
   leetcode::AddTwoNumbers solution;
   actual_output = solution.addTwoNumbers(l1, l2);
-  ASSERT_TRUE(isEqual(expected_output, actual_output));
+  ASSERT_TRUE(leetcode::isEqual(expected_output, actual_output));
 }
 
 TEST_F(AddTwoNumbers, ThirdExample) {
-  l1 = getLinkedListForm({9, 9, 9, 9, 9, 9, 9});
-  l2 = getLinkedListForm({9, 9, 9, 9});
-  expected_output = getLinkedListForm({8, 9, 9, 9, 0, 0, 0, 1});
+  l1 = leetcode::getLinkedListForm({9, 9, 9, 9, 9, 9, 9});
+  l2 = leetcode::getLinkedListForm({9, 9, 9, 9});
+  expected_output = leetcode::getLinkedListForm({8, 9, 9, 9, 0, 0, 0, 1});
   leetcode::AddTwoNumbers solution;
   actual_output = solution.addTwoNumbers(l1, l2);
-  ASSERT_TRUE(isEqual(expected_output, actual_output));
-}
-
-leetcode::ListNode* getLinkedListForm(std::vector<int> input) {
-  leetcode::ListNode* result = nullptr;
-  for (int i = input.size() - 1; i >= 0; i--) {
-    result = new leetcode::ListNode(input[i], result);
-  }
-  return result;
-}
-
-bool isEqual(leetcode::ListNode* expected, leetcode::ListNode* actual) {
-  for (auto current = expected; current != nullptr; current = current->next) {
-    if (current->val != actual->val) {
-      return false;
-    }
-    actual = actual->next;
-  }
-  return true;
+  ASSERT_TRUE(leetcode::isEqual(expected_output, actual_output));
 }
