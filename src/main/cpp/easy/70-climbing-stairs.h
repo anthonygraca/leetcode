c3ace19ce5168023c90b323ab089faf8ad0d7908
Anthony Graca <anthonygraca@gmail.com>
2021-07-28 22:58:41 -0700

    added second test for 70 and added description

--- src/main/cpp/easy/70-climbing-stairs.h
+++ src/main/cpp/easy/70-climbing-stairs.h
@@ -1,6 +1,17 @@
 #ifndef LEETCODE_EASY_70_CLIMBING_STAIRS_H_
 #define LEETCODE_EASY_70_CLIMBING_STAIRS_H_
 
+/*
+   You are climbing a staircase. It takes n steps to reach the top.
+
+   Each time you can either climb 1 or 2 steps. In how many distinct ways can 
+   you climb to the top?
+
+
+   Constraints:
+   * 1 <= n <= 45
+ */
+
 namespace leetcode {
 class ClimbingStairs {
   public:
--- src/test/cpp/easy/70-climbing-stairs-test.cpp
+++ src/test/cpp/easy/70-climbing-stairs-test.cpp
@@ -2,9 +2,29 @@
 
 #include "gtest/gtest.h"
 
+/*
+ * Explanation:
+ * 1.) 1 step + 1 step
+ * 2.) 2 steps
+ */
 TEST(ClimbingStairs, FirstExample) {
   int input{2};
   leetcode::ClimbingStairs solution;
   int expected{2};
   ASSERT_EQ(expected, solution.climbStairs(input));
 }
+
+/*
+ * Explanation:
+ * 1.) 1 step + 1 step + 1 step
+ * 2.) 1 step + 2 steps
+ * 3.) 2 steps + 1 step
+ */
+/*
+TEST(ClimbingStairs, SecondExample) {
+  int input{3};
+  leetcode::ClimbingStairs solution;
+  int expected{3};
+  ASSERT_EQ(expected, solution.climbStairs(input));
+}
+*/
