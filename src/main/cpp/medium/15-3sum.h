7e5747fd0f1c84fec583355b2b404f4434ae471a
Anthony Graca <anthonygraca@gmail.com>
2021-08-01 17:54:14 -0700

    added description and more tests for 15

--- src/main/cpp/medium/15-3sum.h
+++ src/main/cpp/medium/15-3sum.h
@@ -1,6 +1,19 @@
 #ifndef LEETCODE_MEDIUM_15_THREE_SUM_H_
 #define LEETCODE_MEDIUM_15_THREE_SUM_H_
 
+/*
+  Given an integer array nums, return all the triplets 
+  [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and 
+  nums[i] + nums[j] + nums[k] == 0.
+
+  Notice that the solution set must not contain duplicate triplets.
+
+
+  Constaints:
+  * 0 <= nums.length <= 3000
+  * -10^5 <= nums[i] <= 10^5
+ */
+
 #include <vector>
 
 namespace leetcode {
--- src/test/cpp/medium/15-3sum-test.cpp
+++ src/test/cpp/medium/15-3sum-test.cpp
@@ -8,3 +8,19 @@ TEST(ThreeSum, FirstExample) {
   std::vector<std::vector<int>> expected{{-1, -1, 2}, {-1, 0, 1}};
   ASSERT_EQ(expected, solution.threeSum(nums));
 }
+
+/*
+TEST(ThreeSum, SecondExample) {
+  std::vector<int> nums={};
+  leetcode::ThreeSum solution;
+  std::vector<std::vector<int>> expected{};
+  ASSERT_EQ(expected, solution.threeSum(nums));
+}
+
+TEST(ThreeSum, ThirdExample) {
+  std::vector<int> nums={0};
+  leetcode::ThreeSum solution;
+  std::vector<std::vector<int>> expected{};
+  ASSERT_EQ(expected, solution.threeSum(nums));
+}
+*/
