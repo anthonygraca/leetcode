e5274e53fb0524f9f6e03ed52ae6b1d037262d71
Anthony Graca <anthonygraca@gmail.com>
2021-08-06 22:39:32 -0700

    added description, more tests, and updated readme for 17

--- src/main/cpp/medium/17-letter-combinations-of-a-phone-number.h
+++ src/main/cpp/medium/17-letter-combinations-of-a-phone-number.h
@@ -1,6 +1,19 @@
 #ifndef LEETCODE_MEDIUM_17_LETTER_COMBINATIONS_OF_A_PHONE_NUMBER_H_
 #define LEETCODE_MEDIUM_17_LETTER_COMBINATIONS_OF_A_PHONE_NUMBER_H_
 
+/*
+ * Given a string containing digits from 2-9 inclusive, return all possible 
+ * letter combinations that the number could represent. Return the answer in 
+ * any order.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given 
+ * below. Note that 1 does not map to any letters.
+ *
+ * Constraints:
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ */
+
 #include <string>
 #include <vector>
 
--- src/test/cpp/medium/17-letter-combinations-of-a-phone-number-test.cpp
+++ src/test/cpp/medium/17-letter-combinations-of-a-phone-number-test.cpp
@@ -8,3 +8,19 @@ TEST(LetterCombinationsOfAPhoneNumber, FirstExample) {
   std::vector<std::string> expected{"ad","ae","af","bd","be","bf","cd","ce","cf"};
   ASSERT_EQ(expected, solution.letterCombinations(digits));
 }
+
+/*
+TEST(LetterCombinationsOfAPhoneNumber, SecondExample) {
+  std::string digits{""};
+  leetcode::LetterCombinationsOfAPhoneNumber solution;
+  std::vector<std::string> expected;
+  ASSERT_EQ(expected, solution.letterCombinations(digits));
+}
+
+TEST(LetterCombinationsOfAPhoneNumber, ThirdExample) {
+  std::string digits{"2"};
+  leetcode::LetterCombinationsOfAPhoneNumber solution;
+  std::vector<std::string> expected{"a","b","c"};
+  ASSERT_EQ(expected, solution.letterCombinations(digits));
+}
+*/
