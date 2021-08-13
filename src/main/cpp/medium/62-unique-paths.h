2454809e02040cbfab899a7129d76489e02e23a1
Anthony Graca <anthonygraca@gmail.com>
2021-08-12 22:41:31 -0700

    solved 62

--- src/main/cpp/medium/62-unique-paths.h
+++ src/main/cpp/medium/62-unique-paths.h
@@ -1,11 +1,25 @@
 #ifndef LEETCODE_MEDIUM_62_UNIQUE_PATHS_H_
 #define LEETCODE_MEDIUM_62_UNIQUE_PATHS_H_
 
+#include <vector>
+
 namespace leetcode {
 class UniquePaths {
   public:
     int unique_paths(int m, int n) {
-      return 28;
+      if (m == 1 || n == 1) return 1;
+      std::vector<std::vector<int>> found(m+1, std::vector<int>(n+1, -1));
+      found[1][1] = 0;
+      found[1][2] = 1;
+      found[2][1] = 1;
+      return unique_paths(m, n, found);
+    }
+
+    int unique_paths(int m, int n, std::vector<std::vector<int>>& found) {
+      if (m == 1 || n == 1) return 1;
+      if (found[m][n] != -1) return found[m][n];
+      found[m][n] = unique_paths(m-1, n, found) + unique_paths(m, n-1, found);
+      return found[m][n];
     }
 };
 } // namespace leetcode
