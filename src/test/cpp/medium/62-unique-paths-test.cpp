d8ab3d3f8fbbd345a005d96cac70751849e192f1
Anthony Graca <anthonygraca@gmail.com>
2021-08-12 22:43:30 -0700

    added more examples for 62

--- src/test/cpp/medium/62-unique-paths-test.cpp
+++ src/test/cpp/medium/62-unique-paths-test.cpp
@@ -9,3 +9,27 @@ TEST(UniquePaths, FirstExample) {
   int expected{28};
   ASSERT_EQ(expected, solution.unique_paths(m, n));
 }
+
+TEST(UniquePaths, SecondExample) {
+  int m{3}; 
+  int n{2};
+  leetcode::UniquePaths solution;
+  int expected{3};
+  ASSERT_EQ(expected, solution.unique_paths(m, n));
+}
+
+TEST(UniquePaths, ThirdExample) {
+  int m{7}; 
+  int n{3};
+  leetcode::UniquePaths solution;
+  int expected{28};
+  ASSERT_EQ(expected, solution.unique_paths(m, n));
+}
+
+TEST(UniquePaths, FourthExample) {
+  int m{3}; 
+  int n{3};
+  leetcode::UniquePaths solution;
+  int expected{6};
+  ASSERT_EQ(expected, solution.unique_paths(m, n));
+}
