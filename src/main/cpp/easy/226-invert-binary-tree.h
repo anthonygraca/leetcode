0d0df6ee2210bbdf2709d73f32f3533924fef622
Anthony Graca <anthonygraca@gmail.com>
2021-09-02 21:37:10 -0700

    removed commented out code

--- src/main/cpp/easy/226-invert-binary-tree.h
+++ src/main/cpp/easy/226-invert-binary-tree.h
@@ -24,24 +24,6 @@ class InvertBinaryTree {
       root->right = left;
       return root;
     }
-    /*
-    TreeNode* invertTree(TreeNode* root) {
-      if (root == nullptr) return root;
-      return postorder(root);
-    }
-  TreeNode* postorder(TreeNode* root) {
-    if (root->left != nullptr) postorder(root->left);
-    if (root->right != nullptr) postorder(root->right);
-    root = swapChildren(root);
-    return root;
-  }
-  TreeNode* swapChildren(TreeNode* root) {
-      auto temp = root->left;
-      root->left = root->right;
-      root->right = temp;
-      return root;
-  }
-  */
 };
 } // namespace leetcode
 
