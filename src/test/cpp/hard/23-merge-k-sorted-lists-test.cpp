4c2f465448835757becba178252bb2f490decdf2
Anthony Graca <anthonygraca@gmail.com>
2021-09-04 21:56:38 -0700

    refractored messy repeated cleanup code to one location

--- src/test/cpp/hard/23-merge-k-sorted-lists-test.cpp
+++ src/test/cpp/hard/23-merge-k-sorted-lists-test.cpp
@@ -4,18 +4,28 @@
 
 #include "util/linked-list.h"
 
-TEST(MergeKSortedLists, FirstExample) {
-  std::vector<leetcode::ListNode*> input;
+class MergeKSortedLists : public ::testing::Test {
+  protected:
+    std::vector<leetcode::ListNode*> input;
+    leetcode::ListNode* expected = nullptr;
+    leetcode::ListNode* output = nullptr;
+
+    void TearDown() override {
+      leetcode::releaseNodes(expected);
+      for(auto i : input) {
+        leetcode::releaseNodes(i);
+      }
+      input.clear();
+      leetcode::releaseNodes(output);
+    }
+};
+
+TEST_F(MergeKSortedLists, FirstExample) {
   input.push_back(leetcode::getLinkedListForm({1,4,5}));
   input.push_back(leetcode::getLinkedListForm({1,3,4}));
   input.push_back(leetcode::getLinkedListForm({2,6}));
   leetcode::MergeKSortedLists solution;
-  leetcode::ListNode* expected = leetcode::getLinkedListForm({1,1,2,3,4,4,5,6});
-  leetcode::ListNode* output = solution.mergeKLists(input);
+  expected = leetcode::getLinkedListForm({1,1,2,3,4,4,5,6});
+  output = solution.mergeKLists(input);
   ASSERT_TRUE(leetcode::isEqual(expected, output));
-  leetcode::releaseNodes(expected);
-  for(auto i : input) {
-    leetcode::releaseNodes(i);
-  }
-  leetcode::releaseNodes(output);
 }
