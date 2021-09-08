8bb4b628de97e37d8378986b87450b00729a2ca8
Anthony Graca <anthonygraca@gmail.com>
2021-09-07 21:44:50 -0700

    added second example

--- src/test/cpp/easy/206-reverse-linked-list-test.cpp
+++ src/test/cpp/easy/206-reverse-linked-list-test.cpp
@@ -9,4 +9,15 @@ TEST(ReverseLinkedList, FirstExample) {
   leetcode::ReverseLinkedList solution;
   leetcode::ListNode* expected = leetcode::getLinkedListForm({5,4,3,2,1});
   ASSERT_TRUE(leetcode::isEqual(solution.reverseList(input), expected));
+  leetcode::releaseNodes(input);
+  leetcode::releaseNodes(expected);
+}
+
+TEST(ReverseLinkedList, DISABLED_SecondExample) {
+  leetcode::ListNode* input = leetcode::getLinkedListForm({1,2});
+  leetcode::ReverseLinkedList solution;
+  leetcode::ListNode* expected = leetcode::getLinkedListForm({2,1});
+  ASSERT_TRUE(leetcode::isEqual(solution.reverseList(input), expected));
+  leetcode::releaseNodes(input);
+  leetcode::releaseNodes(expected);
 }
