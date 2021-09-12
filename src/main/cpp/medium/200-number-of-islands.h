4d963dddaec824a8da4c9506252697fc7aa413df
Anthony Graca <anthonygraca@gmail.com>
2021-09-11 21:30:12 -0700

    added problem description

--- src/main/cpp/medium/200-number-of-islands.h
+++ src/main/cpp/medium/200-number-of-islands.h
@@ -1,6 +1,22 @@
 #ifndef LEETCODE_MEDIUM_200_NUMBER_OF_ISLANDS_H_
 #define LEETCODE_MEDIUM_200_NUMBER_OF_ISLANDS_H_
 
+/*
+    Given an m x n 2D binary grid grid which represents a map of '1's (land) 
+    and '0's (water), return the number of islands.
+
+    An island is surrounded by water and is formed by connecting adjacent 
+    lands horizontally or vertically. You may assume all four edges of the 
+    grid are all surrounded by water.
+
+
+    Constraints:
+     * m == grid.length
+     * n == grid[i].length
+     * 1 <= m, n <= 300
+     * grid[i][j] is '0' or '1'.
+ */
+
 #include <vector>
 
 namespace leetcode {
