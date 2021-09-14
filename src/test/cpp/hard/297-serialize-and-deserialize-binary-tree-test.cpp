28976393731c756be7336ee95b9e7af78270570b
Anthony Graca <anthonygraca@gmail.com>
2021-09-14 06:58:24 -0700

    added other test cases

--- src/test/cpp/hard/297-serialize-and-deserialize-binary-tree-test.cpp
+++ src/test/cpp/hard/297-serialize-and-deserialize-binary-tree-test.cpp
@@ -12,7 +12,7 @@
  *             /     \
  *            4       5
  */
-TEST(SerializeAndDeserializeBinaryTree, FirstExample) {
+TEST(SerializeAndDeserializeBinaryTree, FirstExampleSerialize) {
   leetcode::TreeNode deepest_left_child(4);
   leetcode::TreeNode deepest_right_child(5);
   leetcode::TreeNode right_child(3, &deepest_left_child, &deepest_right_child);
@@ -22,3 +22,30 @@ TEST(SerializeAndDeserializeBinaryTree, FirstExample) {
   std::string expected{"1,2,3,4,null,null,4,5"};
   ASSERT_EQ(expected, solution.serialize(&root));
 }
+
+/*
+TEST(SerializeAndDeserializeBinaryTree, SecondExampleSerialize) {
+  leetcode::SerializeAndDeserializeBinaryTree solution;
+  std::string expected{""};
+  ASSERT_EQ(expected, solution.serialize(nullptr));
+}
+*/
+
+/*
+TEST(SerializeAndDeserializeBinaryTree, ThirdExampleSerialize) {
+  leetcode::TreeNode root(1);
+  leetcode::SerializeAndDeserializeBinaryTree solution;
+  std::string expected{"1"};
+  ASSERT_EQ(expected, solution.serialize(&root));
+}
+*/
+
+/*
+TEST(SerializeAndDeserializeBinaryTree, FourthExampleSerialize) {
+  leetcode::TreeNode left_child(2);
+  leetcode::TreeNode root(1, &left_child, nullptr);
+  leetcode::SerializeAndDeserializeBinaryTree solution;
+  std::string expected{"1,2"};
+  ASSERT_EQ(expected, solution.serialize(&root));
+}
+*/
