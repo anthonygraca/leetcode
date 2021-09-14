9dbb036216c1aa19829fe0cc30c10c19600bb1e2
Anthony Graca <anthonygraca@gmail.com>
2021-09-14 07:00:36 -0700

    added problem description for 297

--- src/main/cpp/hard/297-serialize-and-deserialize-binary-tree.h
+++ src/main/cpp/hard/297-serialize-and-deserialize-binary-tree.h
@@ -1,6 +1,28 @@
 #ifndef LEETCODE_HARD_297_SERIALIZE_AND_DESERIALIZE_BINARY_TREE_H_
 #define LEETCODE_HARD_297_SERIALIZE_AND_DESERIALIZE_BINARY_TREE_H_
 
+/*
+ * Serialization is the process of converting a data structure or object into 
+ * a sequence of bits so that it can be stored in a file or memory buffer, or 
+ * transmitted across a network connection link to be reconstructed later in 
+ * the same or another computer environment.
+ * 
+ * Design an algorithm to serialize and deserialize a binary tree. There is no 
+ * restriction on how your serialization/deserialization algorithm should work.
+ * You just need to ensure that a binary tree can be serialized to a string 
+ * and this string can be deserialized to the original tree structure.
+ * 
+ * Clarification: The input/output format is the same as how LeetCode 
+ * serializes a binary tree. You do not necessarily need to follow this 
+ * format, so please be creative and come up with different approaches 
+ * yourself.
+ *
+ *
+ * Constraints:
+ *  * The number of nodes in the tree is in the range [0, 10^4].
+ *  * -1000 <= Node.val <= 1000
+ */
+
 #include <string>
 
 #include "util/tree-node.h"
