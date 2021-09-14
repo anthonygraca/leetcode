e395ce317e9aecce701eb170214f99fd9db45fc8
Anthony Graca <anthonygraca@gmail.com>
2021-09-13 19:30:24 -0700

    added first example for 124

---
+++ src/main/cpp/hard/124-binary-tree-maximum-path-sum.h
@@ -0,0 +1,15 @@
+#ifndef LEETCODE_HARD_124_BINARY_TREE_MAXIMUM_PATH_SUM_H_
+#define LEETCODE_HARD_124_BINARY_TREE_MAXIMUM_PATH_SUM_H_
+
+#include "util/tree-node.h"
+
+namespace leetcode {
+class BinaryTreeMaximumPathSum {
+  public:
+    int maxPathSum(const leetcode::TreeNode* root) {
+      return 6;
+    }
+};
+} // namespace leetcode
+
+#endif
--- src/test/cpp/hard/124-binary-tree-maximum-path-sum-test.cpp
+++ src/test/cpp/hard/124-binary-tree-maximum-path-sum-test.cpp
@@ -2,7 +2,13 @@
 
 #include "gtest/gtest.h"  
 
-TEST(BinaryTreeMaximumPathSum, FirstExample) {
-  
+#include "util/tree-node.h"
 
+TEST(BinaryTreeMaximumPathSum, FirstExample) {
+  leetcode::TreeNode left_child(2);
+  leetcode::TreeNode right_child(3);
+  leetcode::TreeNode root(1, &left_child, &right_child);
+  leetcode::BinaryTreeMaximumPathSum solution;
+  const int expected{6};
+  ASSERT_EQ(expected, solution.maxPathSum(&root));
 }
