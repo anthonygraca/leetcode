36a6cc532c30220c261714974f1fab317f2c3928
Anthony Graca <anthonygraca@gmail.com>
2021-09-24 22:25:06 -0700

    implemented second level

--- src/main/cpp/easy/104-maximum-depth-of-binary-tree.h
+++ src/main/cpp/easy/104-maximum-depth-of-binary-tree.h
@@ -1,6 +1,8 @@
 #ifndef LEETCODE_EASY_104_MAXIMUM_DEPTH_OF_BINARY_TREE_H_
 #define LEETCODE_EASY_104_MAXIMUM_DEPTH_OF_BINARY_TREE_H_
 
+#include <algorithm>
+
 #include "util/tree-node.h"
 
 namespace leetcode {
@@ -8,9 +10,17 @@ class MaximumDepthOfBinaryTree {
   public:
     int maxDepth(TreeNode* node) {
       if (node == nullptr) {
-        return 0;
+        return m_max_depth;
       }
-      return 1;
+      inorder(node, m_max_depth+1);
+      return m_max_depth;
+    }
+  private:
+    int m_max_depth = 0;
+    void inorder(TreeNode* node, int current_depth) {
+      if (node->left != nullptr) inorder(node->left, current_depth+1);
+      m_max_depth = std::max(m_max_depth, current_depth);
+      if (node->right != nullptr) inorder(node->right, current_depth+1);
     }
 };
 } // namespace leetcode
--- src/test/cpp/easy/104-maximum-depth-of-binary-tree-test.cpp
+++ src/test/cpp/easy/104-maximum-depth-of-binary-tree-test.cpp
@@ -4,13 +4,37 @@
 
 #include "util/tree-node.h"
 
+const int SOME_NUMBER{8};
+
 TEST(MaximumDepthOfBinaryTree, NullExample) {
   leetcode::MaximumDepthOfBinaryTree solution;
   ASSERT_EQ(0, solution.maxDepth(nullptr));
 }
 
-TEST(MaximumDepthOfBinaryTree, SingleLevelExample) {
-  leetcode::TreeNode node{8};
+TEST(MaximumDepthOfBinaryTree, FirstLevelExample) {
+  leetcode::TreeNode node{SOME_NUMBER};
   leetcode::MaximumDepthOfBinaryTree solution;
   ASSERT_EQ(1, solution.maxDepth(&node));
+}
+
+TEST(MaximumDepthOfBinaryTree, SecondLevelExampleLeftLeaning) {
+  leetcode::TreeNode left_node{SOME_NUMBER};
+  leetcode::TreeNode node{SOME_NUMBER, &left_node, nullptr};
+  leetcode::MaximumDepthOfBinaryTree solution;
+  ASSERT_EQ(2, solution.maxDepth(&node));
+}
+
+TEST(MaximumDepthOfBinaryTree, SecondLevelExampleRightLeaning) {
+  leetcode::TreeNode right_node{SOME_NUMBER};
+  leetcode::TreeNode node{SOME_NUMBER, nullptr, &right_node};
+  leetcode::MaximumDepthOfBinaryTree solution;
+  ASSERT_EQ(2, solution.maxDepth(&node));
+}
+
+TEST(MaximumDepthOfBinaryTree, SecondLevelExampleBalanced) {
+  leetcode::TreeNode left_node{SOME_NUMBER};
+  leetcode::TreeNode right_node{SOME_NUMBER};
+  leetcode::TreeNode node{SOME_NUMBER, &left_node, &right_node};
+  leetcode::MaximumDepthOfBinaryTree solution;
+  ASSERT_EQ(2, solution.maxDepth(&node));
 }
