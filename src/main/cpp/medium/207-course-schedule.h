7ae32775ab39a56edde5c2d34579be35b0d31bcc
Anthony Graca <anthonygraca@gmail.com>
2021-10-13 22:06:26 -0700

    added test infrastructure and started 207: course schedule

---
+++ src/main/cpp/medium/207-course-schedule.h
@@ -0,0 +1,15 @@
+#ifndef LEETCODE_MEDIUM_207_COURSE_SCHEDULE_H_
+#define LEETCODE_MEDIUM_207_COURSE_SCHEDULE_H_
+
+#include <vector>
+
+namespace leetcode {
+class CourseSchedule {
+public:
+  bool canFinish(int num_courses, std::vector<std::vector<int>> prerequisites) {
+    return true;
+  }
+};
+} // namespace leetcode
+
+#endif
---
+++ src/test/cpp/medium/207-course-schedule-test.cpp
@@ -0,0 +1,12 @@
+#include "medium/207-course-schedule.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+TEST(CourseSchedule, FirstExample) {
+  int num_courses{2};
+  std::vector<std::vector<int>> prerequisites{{1,0}};
+  leetcode::CourseSchedule algo;
+  ASSERT_TRUE(algo.canFinish(num_courses, prerequisites));
+}
