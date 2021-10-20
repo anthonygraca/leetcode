1817bcfd4b8df2bd8203c32da533f67732c0cb67
Anthony Graca <anthonygraca@gmail.com>
2021-10-19 19:26:14 -0700

    solved 46 w/ heap's algo

--- src/main/cpp/medium/46-permutations.h
+++ src/main/cpp/medium/46-permutations.h
@@ -6,12 +6,28 @@
 namespace leetcode {
 class Permutation {
 public:
-  std::vector<std::vector<int>> permute(std::vector<int> nums) {
-    std::vector<std::vector<int>> expected{{1,2,3}, {1,3,2}, 
-                                         {2,1,3}, {2,3,1}, 
-                                         {3,1,2}, {3,2,1} };
-    return expected;
+  std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+    permute(nums, nums.size());
+    return solution;
   }
+  void permute(std::vector<int>& nums, int k) {
+    if (k == 1) solution.push_back(std::vector{nums});
+    else {
+      permute(nums, k-1);
+
+      for(int i = 0; i < k-1; i++) {
+        if (k % 2 == 0) {
+          std::swap(nums[i], nums[k-1]);
+        }
+        else {
+          std::swap(nums[0], nums[k-1]);
+        }
+        permute(nums, k-1);
+      }
+    }
+  }
+private:
+  std::vector<std::vector<int>> solution;
 };
 }
 
--- src/test/cpp/medium/46-permutations-test.cpp
+++ src/test/cpp/medium/46-permutations-test.cpp
@@ -7,8 +7,22 @@
 TEST(Permutations, FirstExample) {
   std::vector<int> nums{1,2,3};
   leetcode::Permutation algo;
-  std::vector<std::vector<int>> expected{{1,2,3}, {1,3,2}, 
-                                         {2,1,3}, {2,3,1}, 
-                                         {3,1,2}, {3,2,1} };
+  std::vector<std::vector<int>> expected{{1,2,3}, {2,1,3},
+                                         {3,1,2}, {1,3,2}, 
+                                         {2,3,1}, {3,2,1} };
+  ASSERT_EQ(expected, algo.permute(nums));
+}
+
+TEST(Permutations, SecondExample) {
+  std::vector<int> nums{0,1};
+  leetcode::Permutation algo;
+  std::vector<std::vector<int>> expected{{0,1}, {1,0}};
+  ASSERT_EQ(expected, algo.permute(nums));
+}
+
+TEST(Permutations, ThirdExample) {
+  std::vector<int> nums{1};
+  leetcode::Permutation algo;
+  std::vector<std::vector<int>> expected{{1}};
   ASSERT_EQ(expected, algo.permute(nums));
 }
