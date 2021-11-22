243b15dcbb91255cfccff8a8f4f2b83a62d7d704
Anthony Graca <anthonygraca@gmail.com>
2021-11-21 21:07:45 -0800

    added extra examples and description to 1691

--- src/main/cpp/hard/1691-maximum-height-by-stacking-cuboids.h
+++ src/main/cpp/hard/1691-maximum-height-by-stacking-cuboids.h
@@ -1,6 +1,25 @@
 #ifndef LEETCODE_HARD_1691_MAXIMUM_HEIGHT_BY_STACKING_CUBOIDS_H_
 #define LEETCODE_HARD_1691_MAXIMUM_HEIGHT_BY_STACKING_CUBOIDS_H_
 
+/*
+  Given n cuboids where the dimensions of the ith cuboid is 
+  cuboids[i] = [widthi, lengthi, heighti] (0-indexed). Choose a subset of 
+  cuboids and place them on each other.
+
+  You can place cuboid i on cuboid j if width_i <= width_j and length_i <= length_j 
+  and height_i <= height_j. You can rearrange any cuboid's dimensions by 
+  rotating it to put it on another cuboid.
+
+  Return the maximum height of the stacked cuboids.
+
+
+  Constraints:
+  * n == cuboids.length
+  * 1 <= n <= 100
+  * 1 <= width_i, length_i, height_i <= 100
+ */
+
+
 #include <vector>
 
 namespace leetcode {
--- src/test/cpp/hard/1691-maximum-height-by-stacking-cuboids-test.cpp
+++ src/test/cpp/hard/1691-maximum-height-by-stacking-cuboids-test.cpp
@@ -4,8 +4,38 @@
 
 #include <vector>
 
+/*
+ * Explanation:
+ * Cuboid 1 is placed on the bottom with the 53x37 side facing down with height 95.
+ * Cuboid 0 is placed next with the 45x20 side facing down with height 50.
+ * Cuboid 2 is placed next with the 23x12 side facing down with height 45.
+ * The total height is 95 + 50 + 45 = 190.
+ */
 TEST(MaximumHeightByStackingCuboids, FirstExample) {
   std::vector<std::vector<int>> cuboids{{50,45,20},{95,37,53},{45,23,12}};
   leetcode::MaximumHeightByStackingCuboids algo;
   ASSERT_EQ(190, algo.maxHeight(cuboids));
+}
+
+/*
+ * Explanation:
+ * You can't place any of the cuboids on the other.
+ * We choose cuboid 1 and rotate it so that the 35x3 side is facing down and its height is 76.
+ */
+TEST(MaximumHeightByStackingCuboids, DISABLED_SecondExample) {
+  std::vector<std::vector<int>> cuboids{{38,25,45},{76,35,3}};
+  leetcode::MaximumHeightByStackingCuboids algo;
+  ASSERT_EQ(76, algo.maxHeight(cuboids));
+}
+
+/*
+ * Explanation:
+ * After rearranging the cuboids, you can see that all cuboids have the same dimension.
+ * You can place the 11x7 side down on all cuboids so their heights are 17.
+ * The maximum height of stacked cuboids is 6 * 17 = 102.
+ */
+TEST(MaximumHeightByStackingCuboids, DISABLED_ThirdExample) {
+  std::vector<std::vector<int>> cuboids{{7,11,17},{7,17,11},{11,7,17},{11,17,7},{17,7,11},{17,11,7}};
+  leetcode::MaximumHeightByStackingCuboids algo;
+  ASSERT_EQ(102, algo.maxHeight(cuboids));
 }
